1c2d632ca6305f1799c4af57b4e5fd3a3bd75044
Rafael Vaz <ferreira.vaz@live.com>
2026-08-21 10:37:41 -0400

    feat: create authRoutes and register endpoint

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 8 */
+/*jshint esversion: 8 */  
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -22,25 +22,19 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-const giftRoutes = require('./routes/giftRoutes'); 
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
+const giftRoutes = require('./routes/giftRoutes');
+const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
-
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-app.use('/api/gifts', giftRoutes); 
+app.use('/api/gifts', giftRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/search', searchRoutes);
 
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('/api/search', searchRoutes); 
-
- 
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
@@ -54,3 +48,4 @@ app.get("/",(req,res)=>{
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+ 
